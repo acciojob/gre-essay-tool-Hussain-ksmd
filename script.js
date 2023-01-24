bd955d82411f532ebbe17caf6030b85abded8e43
Mohammad Hussain <95843547+Hussain-ksmd@users.noreply.github.com>
2023-01-24 12:00:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+// Get the textbox and h3 tag
+      const textbox = document.getElementById("evaluatedText");
+      const wordCount = document.getElementById("wordCount");
+Copy code
+  // Add an event listener to the textbox to listen for changes
+  textbox.addEventListener("input", function() {
+    // Get the text from the textbox
+    const text = textbox.value;
+    
+    // Count the number of words in the text
+    const count = text.trim().split(" ").length;
+    
+    // Update the h3 tag with the new word count
+    wordCount.innerHTML = count;
+  });
